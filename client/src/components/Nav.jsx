60f7173682e978c92bcebbb7014c7e2ea7e8e5e4
Justin Elmourne <jelmourne@outlook.com>
2024-01-01 18:14:08 -0500

    Cart

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,51 +1,57 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from 'react';
-import { Avatar, Dropdown, Navbar, Button } from 'flowbite-react';
-import SearchBar from './SeachBar';
-import LoginDropdown from './LoginDropdown';
-import LoginForm from './LoginForm';
-import { Link } from 'react-router-dom';
+import React, { Component, useState } from "react";
+import { Avatar, Dropdown, Navbar, Button } from "flowbite-react";
+import SearchBar from "./SeachBar";
+import LoginDropdown from "./LoginDropdown";
+import LoginForm from "./LoginForm";
+import { Link } from "react-router-dom";
+import CartList from "./cart/CartList";
 
 const Nav = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <Navbar fluid className='bg-brand-darkgrey'>
-        <Navbar.Brand href='#'>
+      {open && <CartList onClick={setOpen} />}
+      <Navbar fluid className="bg-brand-darkgrey">
+        <Navbar.Brand href="#">
           <img
-            src='logo.svg'
-            className='ml-3 mr-3 h-6 sm:h-9'
-            alt='VinylVault Logo'
+            src="logo.svg"
+            className="ml-3 mr-3 h-6 sm:h-9"
+            alt="VinylVault Logo"
           />
-          <span className='self-center whitespace-nowrap text-l font-semibold text-white'>
+          <span className="self-center whitespace-nowrap text-l font-semibold text-white">
             VinylVault
           </span>
         </Navbar.Brand>
-        <div className='flex gap-2 md:order-2 mr-3'>
+        <div className="flex gap-2 md:order-2 mr-3">
           <SearchBar></SearchBar>
           <Dropdown
             arrowIcon={false}
             inline
-            className='bg-brand-darkgrey'
-            label={<Avatar alt='User settings' img='user-icon.svg' size='xs' />}
+            className="bg-brand-darkgrey"
+            label={<Avatar alt="User settings" img="user-icon.svg" size="xs" />}
           >
             <div>
-              <Link to='/login'>
+              <Link to="/login">
                 <Button>LOGIN</Button>
               </Link>
-              <Link to='/register'>
+              <Link to="/register">
                 <Button>REGISTER</Button>
               </Link>
             </div>
           </Dropdown>
           <img
-            src='cart-icon.svg'
-            alt='Cart icon'
-            width='25px'
-            height='25px'
+            onClick={() => {
+              setOpen(!open);
+            }}
+            src="cart-icon.svg"
+            alt="Cart icon"
+            width="25px"
+            height="25px"
           ></img>
         </div>
       </Navbar>
-      <div className='h-8 text-white px-40 flex justify-between items-center bg-[#8CBEB270]'>
+      <div className="h-8 text-white px-40 flex justify-between items-center bg-[#8CBEB270]">
         <p>Genre</p>
         <p>Trending</p>
         <p>Recent</p>
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,58 +1,35 @@
 /* eslint-disable no-unused-vars */
-import React, { Component, useState, useEffect } from 'react';
-import Nav from '../components/Nav';
-import '../index.css';
-import CategoryList from '../components/CategoryList';
-import starIcon from '../../public/star-icon.svg';
-import flamesIcon from '../../public/flames-icon.svg';
-import purpleHeart from '../../public/purpleheart-icon.svg';
-import vinylList from '../vinyls.json';
-import axios from 'axios';
+import React, { Component, useState, useEffect } from "react";
+import Nav from "../components/Nav";
+import "../index.css";
+import CategoryList from "../components/categories/CategoryList";
+import starIcon from "../../public/star-icon.svg";
+import flamesIcon from "../../public/flames-icon.svg";
+import purpleHeart from "../../public/purpleheart-icon.svg";
+import axios from "axios";
+
+const urls = [
+  "http://localhost:5050/products/recent?limit=50",
+  "http://localhost:5050/products/trending?limit=50",
+  "http://localhost:5050/products/staff",
+];
 
 const HomeScreen = () => {
-  const [recentVinyls, setRecentVinyls] = useState([]);
-  const [trendingVinyls, setTrendingVinyls] = useState([]);
-
-  // Get Recent Vinyls
-  useEffect(() => {
-    axios
-      .get('http://localhost:5050/products/recent')
-      .then((res) => {
-        setRecentVinyls(res.data.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    axios
-      .get('http://localhost:5050/products/trending')
-      .then((res) => {
-        setTrendingVinyls(res.data.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
   return (
-    <div className='h-full homescreen-bg'>
+    <div className="h-full homescreen-bg">
       <Nav></Nav>
-      <div className='w-11/12 m-auto py-16 h-full bg-brand-darkgrey px-24 flex flex-col gap-y-20'>
+      <div className="w-11/12 h-full m-auto py-16 bg-brand-darkgrey px-24 flex flex-col gap-y-20">
+        <CategoryList category={"Newly Added"} icon={starIcon} url={urls[0]} />
         <CategoryList
-          category={'Newly Added'}
-          icon={starIcon}
-          vinyls={recentVinyls}
-        />
-        <CategoryList
-          category={'Weekly Trends'}
+          category={"Weekly Trends"}
           icon={flamesIcon}
-          vinyls={trendingVinyls}
+          url={urls[1]}
         />
-        {/* <CategoryList
-          category={'Staff Picks'}
+        <CategoryList
+          category={"Staff Picks"}
           icon={purpleHeart}
-          vinyls={trendingVinyls}
-        /> */}
+          url={urls[2]}
+        />
       </div>
     </div>
   );
