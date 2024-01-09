268e04ba0c7c5edaf901ec341e497e8c4ecbe1a7
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-01-09 15:52:00 -0500

    Path

--- server/app.js
+++ server/app.js
@@ -20,7 +20,9 @@ process.on('uncaughtException', function (error) {
 });
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(
+  '/home/ubuntu/transactional-web-project-tristandasilva'
+);
 
 // to redirect frontend
 app.use(express.static(path.join(__dirname, '../client/dist')));
