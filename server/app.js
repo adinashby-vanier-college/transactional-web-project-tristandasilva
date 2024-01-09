7d6dd0e86802d2e337476b8a7939278e4dedf069
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-01-09 16:30:25 -0500

    Configuration

--- server/app.js
+++ server/app.js
@@ -23,9 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // to redirect frontend
-app.use(
-  express.static(path.join(__dirname, '..', 'client', 'dist', 'index.html'))
-);
+app.use(express.static(path.join(__dirname, '..', 'client')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '..', 'client', 'dist', 'index.html'));
