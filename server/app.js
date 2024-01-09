23fc7f726b2d092d684427cee9786b19d5c2801b
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-01-09 10:53:06 -0500

    Port

--- server/app.js
+++ server/app.js
@@ -1,30 +1,30 @@
-import express, { response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import express, { response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-import { router as UserRouter } from "./api/users.js";
-import { router as ProductRouter } from "./api/products.js";
-import { router as CartRouter } from "./api/carts.js";
+import { router as UserRouter } from './api/users.js';
+import { router as ProductRouter } from './api/products.js';
+import { router as CartRouter } from './api/carts.js';
 
-import db from "./config/db.js";
+import db from './config/db.js';
 
 db();
 
 const app = express();
 
-process.on("uncaughtException", function (error) {
+process.on('uncaughtException', function (error) {
   console.log(error.stack);
 });
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
 app.use(cookieParser());
 
-app.use("/users", UserRouter);
-app.use("/products", ProductRouter);
-app.use("/cart", CartRouter);
+app.use('/users', UserRouter);
+app.use('/products', ProductRouter);
+app.use('/cart', CartRouter);
 //app.use("/wishlist")
 
 app.listen(process.env.PORT);
-console.log("Server is listening");
+console.log('Server is listening on port' + process.env.PORT);
