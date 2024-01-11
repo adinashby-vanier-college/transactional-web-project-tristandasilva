dbe78ccb7cbabf7ffc913f3038d2e244d5c8c5b0
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-01-11 17:29:44 -0500

    Update search bar

--- client/src/components/SeachBar.jsx
+++ client/src/components/SeachBar.jsx
@@ -1,11 +1,9 @@
 import React, { Component, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import searchIcon from '../assets/search-icon.svg';
 
 const SearchBar = () => {
-  const [searchInput, setSearchInput] = useState("");
-
-  console.log(searchInput);
+  const [searchInput, setSearchInput] = useState('');
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -13,20 +11,20 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="flex m-1 items-center bg-[#353535] rounded w-[200px]">
+    <div className='flex m-1 items-center bg-[#353535] rounded w-[200px]'>
       <input
-        type="text"
-        placeholder="Search"
+        type='text'
+        placeholder='Search'
         onChange={handleChange}
         value={searchInput}
-        className="bg-[#00000000] border-none w-10/12 text-[#ffffff70]"
+        className='bg-[#00000000] border-none w-10/12 text-[#ffffff70]'
       ></input>
-      <Link to={`/search${"?q=" + searchInput}`}>
+      <Link to={`/search${'?q=' + searchInput}`}>
         <img
           src={searchIcon}
-          alt="Search bar icon"
-          width="25px"
-          height="25px"
+          alt='Search bar icon'
+          width='25px'
+          height='25px'
         ></img>
       </Link>
     </div>
