cb82d4710abe7f4146c1682f5d84443f60313110
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-01-11 17:26:33 -0500

    Updating cors and baseUrl file

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express, { response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { isInProdMode } from '../client/baseUrl.js';
 
 import { router as UserRouter } from './api/users.js';
 import { router as ProductRouter } from './api/products.js';
@@ -14,6 +15,9 @@ db();
 const app = express();
 const _dirname = path.dirname('');
 const buildPath = path.join(_dirname, '../client/dist');
+const corsOrigin = isInProdMode()
+  ? 'http://99.79.60.159:5050'
+  : 'http://localhost:5173';
 
 process.on('uncaughtException', function (error) {
   console.log(error.stack);
@@ -22,7 +26,7 @@ process.on('uncaughtException', function (error) {
 app.use(express.static(buildPath));
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: 'http://99.79.60.159:5050' }));
+app.use(cors({ credentials: true, origin: corsOrigin }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
