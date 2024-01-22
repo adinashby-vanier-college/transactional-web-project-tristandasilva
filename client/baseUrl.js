98dc950b88d11c05d2308a0cf51badea181478ba
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-01-22 14:28:16 -0500

    Update baseUrl.js

--- client/baseUrl.js
+++ client/baseUrl.js
@@ -5,7 +5,7 @@ export const isInProdMode = () => {
 };
 
 const baseUrl = isInProdMode()
-  ? 'http://99.79.60.159'
+  ? 'http://35.183.197.147'
   : 'http://localhost:5050';
 
 export default baseUrl;
