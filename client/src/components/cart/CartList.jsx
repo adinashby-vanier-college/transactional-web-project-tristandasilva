5f081a6c34b06b9c95a47e31536842e6fe38b0c7
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-02-01 08:24:34 -0500

    Update

--- client/src/components/cart/CartList.jsx
+++ client/src/components/cart/CartList.jsx
@@ -1,24 +1,25 @@
 /* eslint-disable react/prop-types */
-import useQuery from "../../hooks/useQuery";
-import { useState } from "react";
-import CartItemList from "./CartItemList";
-import minusIcon from "../../assets/minus.svg";
-import tagIcon from "../../assets/tag.svg";
-import { useTranslation } from "react-i18next";
+import useQuery from '../../hooks/useQuery';
+import { useState } from 'react';
+import CartItemList from './CartItemList';
+import minusIcon from '../../assets/minus.svg';
+import tagIcon from '../../assets/tag.svg';
+import { useTranslation } from 'react-i18next';
+import baseUrl from '../../../baseUrl';
 
 function CartList(props) {
-  const response = useQuery("/cart");
+  const response = useQuery('/cart');
   const [total, setTotal] = useState(0);
   const [t, i18n] = useTranslation();
 
   return (
-    <div className=" bg-neutral-800 text-white h-screen fixed w-1/4 right-0 z-50 flex flex-col animate-slide-r  lg:w-full">
-      <div className="flex justify-between items-center border-b-2 p-5">
-        <div className="flex  text-2xl items-center">
-          {t("cart.cart")}&nbsp;<img src={tagIcon}></img>
+    <div className=' bg-neutral-800 text-white h-screen fixed w-1/4 right-0 z-50 flex flex-col animate-slide-r  lg:w-full'>
+      <div className='flex justify-between items-center border-b-2 p-5'>
+        <div className='flex  text-2xl items-center'>
+          {t('cart.cart')}&nbsp;<img src={tagIcon}></img>
         </div>
         <button
-          className="aspect-square h-10 cursor-pointer"
+          className='aspect-square h-10 cursor-pointer'
           onClick={() => {
             props.onClick(false);
           }}
@@ -27,18 +28,18 @@ function CartList(props) {
         </button>
       </div>
       {response && response.data.empty ? (
-        <div className="flex items-center justify-center h-full ">
-          {t("cart.cartEmpty")}
+        <div className='flex items-center justify-center h-full '>
+          {t('cart.cartEmpty')}
         </div>
       ) : (
         <CartItemList data={response} total={total} setTotal={setTotal} />
       )}
-      <div className="flex items-center justify-between border-t-2 p-5">
-        <p className=" text-2xl">Total: $ {parseFloat(total).toFixed(2)}</p>
-        <form action={`${baseUrl}/checkout`} method="POST">
+      <div className='flex items-center justify-between border-t-2 p-5'>
+        <p className=' text-2xl'>Total: $ {parseFloat(total).toFixed(2)}</p>
+        <form action={`${baseUrl}/checkout`} method='POST'>
           <button
-            className="bg-yellow-500 rounded-lg p-2 px-10 text-xl"
-            type="submit"
+            className='bg-yellow-500 rounded-lg p-2 px-10 text-xl'
+            type='submit'
           >
             Checkout
           </button>
--- server/api/users.js
+++ server/api/users.js
@@ -1,40 +1,40 @@
-import express from "express";
-import User from "../models/user.js";
-import dotenv from "dotenv";
+import express from 'express';
+import User from '../models/user.js';
+import dotenv from 'dotenv';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-} from "firebase/auth";
-import admin from "firebase-admin";
-import firebaseConfig from "../config/firebase.js";
+} from 'firebase/auth';
+import admin from 'firebase-admin';
+import firebaseConfig from '../config/firebase.js';
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' });
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const userEmail = auth.currentUser.email;
   try {
     const user = await User.find({ email: userEmail });
     res.send(user);
   } catch (error) {
-    res.send("No user found");
+    res.send('No user found');
   }
 });
 
-router.get("/logout", async (req, res) => {
-  res.clearCookie("token");
-  res.clearCookie("user");
+router.get('/logout', async (req, res) => {
+  res.clearCookie('token');
+  res.clearCookie('user');
   signOut(auth);
 });
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   const { first_name, last_name, email, password } = req.body;
   try {
     createUserWithEmailAndPassword(auth, email, password).then(
       async (userCred) => {
-        user = userCred.user;
+        let user = userCred.user;
         const newUser = await User.create({
           first_name: first_name,
           last_name: last_name,
@@ -47,12 +47,12 @@ router.post("/register", async (req, res) => {
     err.code == 11000 // This code is for duplicate email
       ? res
           .status(401)
-          .send({ message: "User already associated with this email." })
-      : res.status(400).send({ message: "Registration unsuccessful" });
+          .send({ message: 'User already associated with this email.' })
+      : res.status(400).send({ message: 'Registration unsuccessful' });
   }
 });
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
@@ -63,20 +63,20 @@ router.post("/login", async (req, res) => {
     });
   } catch (err) {
     res.status(400).send({
-      message: "No user assiocated with this email.",
+      message: 'No user assiocated with this email.',
     });
   }
 });
 
-router.post("/login/oauth", async (req, res) => {
+router.post('/login/oauth', async (req, res) => {
   const { token, username, email } = req.body;
 
   try {
     const user = await User.findOne({ email: email });
     if (!user) {
       const newUser = await User.create({
-        first_name: username.split(" ")[0],
-        last_name: username.split(" ")[1],
+        first_name: username.split(' ')[0],
+        last_name: username.split(' ')[1],
         email: email,
       });
       newUser.save();
@@ -87,6 +87,6 @@ router.post("/login/oauth", async (req, res) => {
     console.log(err);
   }
 });
-router.post("/login/facebook", async (req, res) => {});
+router.post('/login/facebook', async (req, res) => {});
 
 export { router };
